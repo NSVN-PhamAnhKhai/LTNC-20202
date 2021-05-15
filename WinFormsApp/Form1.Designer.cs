0795c94e039de8378abaab7e4274ae1ace500af6
anhkhai0107 <anhkhai0107@gmail.com>
2021-05-16 00:19:32 +0700

    'WinFormApp'

---
+++ WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLED1 = new System.Windows.Forms.Button();
+            this.btnLED2 = new System.Windows.Forms.Button();
+            this.btnLED3 = new System.Windows.Forms.Button();
+            this.btnLED4 = new System.Windows.Forms.Button();
+            this.btnAllControl = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLED1
+            // 
+            this.btnLED1.BackColor = System.Drawing.Color.Gray;
+            this.btnLED1.Location = new System.Drawing.Point(129, 164);
+            this.btnLED1.Name = "btnLED1";
+            this.btnLED1.Size = new System.Drawing.Size(94, 29);
+            this.btnLED1.TabIndex = 0;
+            this.btnLED1.Text = "LED1";
+            this.btnLED1.UseVisualStyleBackColor = false;
+            this.btnLED1.Click += new System.EventHandler(this.btnLED1_Click);
+            // 
+            // btnLED2
+            // 
+            this.btnLED2.BackColor = System.Drawing.Color.Gray;
+            this.btnLED2.Location = new System.Drawing.Point(278, 164);
+            this.btnLED2.Name = "btnLED2";
+            this.btnLED2.Size = new System.Drawing.Size(94, 29);
+            this.btnLED2.TabIndex = 0;
+            this.btnLED2.Text = "LED2";
+            this.btnLED2.UseVisualStyleBackColor = false;
+            this.btnLED2.Click += new System.EventHandler(this.btnLED2_Click);
+            // 
+            // btnLED3
+            // 
+            this.btnLED3.BackColor = System.Drawing.Color.Gray;
+            this.btnLED3.Location = new System.Drawing.Point(402, 164);
+            this.btnLED3.Name = "btnLED3";
+            this.btnLED3.Size = new System.Drawing.Size(94, 29);
+            this.btnLED3.TabIndex = 0;
+            this.btnLED3.Text = "LED3";
+            this.btnLED3.UseVisualStyleBackColor = false;
+            this.btnLED3.Click += new System.EventHandler(this.btnLED3_Click);
+            // 
+            // btnLED4
+            // 
+            this.btnLED4.BackColor = System.Drawing.Color.Gray;
+            this.btnLED4.Location = new System.Drawing.Point(517, 164);
+            this.btnLED4.Name = "btnLED4";
+            this.btnLED4.Size = new System.Drawing.Size(94, 29);
+            this.btnLED4.TabIndex = 0;
+            this.btnLED4.Text = "LED4";
+            this.btnLED4.UseVisualStyleBackColor = false;
+            this.btnLED4.Click += new System.EventHandler(this.btnLED4_Click);
+            // 
+            // btnAllControl
+            // 
+            this.btnAllControl.BackColor = System.Drawing.Color.Silver;
+            this.btnAllControl.Location = new System.Drawing.Point(657, 164);
+            this.btnAllControl.Name = "btnAllControl";
+            this.btnAllControl.Size = new System.Drawing.Size(94, 29);
+            this.btnAllControl.TabIndex = 0;
+            this.btnAllControl.Text = "All ON";
+            this.btnAllControl.UseVisualStyleBackColor = false;
+            this.btnAllControl.Click += new System.EventHandler(this.btnAllControl_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(402, 276);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 29);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAllControl);
+            this.Controls.Add(this.btnLED4);
+            this.Controls.Add(this.btnLED3);
+            this.Controls.Add(this.btnLED2);
+            this.Controls.Add(this.btnLED1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLED1;
+        private System.Windows.Forms.Button btnLED2;
+        private System.Windows.Forms.Button btnLED3;
+        private System.Windows.Forms.Button btnLED4;
+        private System.Windows.Forms.Button btnAllControl;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ WinFormsApp/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static MqttClient client;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                client = new MqttClient("broker.emqx.io", 1883, false, MqttSslProtocols.None, null, null);
+                client.ProtocolVersion = MqttProtocolVersion.Version_3_1;
+                byte code = client.Connect(Guid.NewGuid().ToString());
+                if (code == 0)
+                {
+                    MessageBox.Show(this, "Connect Successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+                    //Subcribe Topic
+                    client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+                    client.Subscribe(new string[] {"Mobile/LEDControl", "Web/LEDControl", "Hardware/LEDControl"}, new byte[] {1, 1, 1});
+                }
+
+                else MessageBox.Show(this, "Connect Fail", "Message", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show(this, "Wrong Format", "Message", MessageBoxButtons.OK, MessageBoxIcon.Question);
+            }
+        }
+
+        Action<string> ReceiveAction;
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            ReceiveAction = Receive;
+            try
+            {
+                this.BeginInvoke(ReceiveAction, Encoding.UTF8.GetString(e.Message));
+            }
+            catch { };
+        }
+
+        void Receive(string message)
+        {
+
+            int status = int.Parse(message);
+            LEDControl(status);
+            return;
+        }
+
+        void LEDControl(int num)
+        {
+            int status = num % 10;
+            int led = num / 10;
+            switch (led)
+            {
+                case 0:
+                    if (status == 0)
+                    {
+                        btnLED1.BackColor = Color.Gray;
+                        btnLED2.BackColor = Color.Gray;
+                        btnLED3.BackColor = Color.Gray;
+                        btnLED4.BackColor = Color.Gray;
+                        btnAllControl.Text = "All ON";
+                    }
+                    else
+                    {
+                        btnLED1.BackColor = Color.GreenYellow;
+                        btnLED2.BackColor = Color.GreenYellow;
+                        btnLED3.BackColor = Color.GreenYellow;
+                        btnLED4.BackColor = Color.GreenYellow;
+                        btnAllControl.Text = "All OFF";
+                    }
+                    break;
+                case 1:
+                    if (status == 0)
+                    {
+                        btnLED1.BackColor = Color.Gray;
+                    }
+                    else
+                    {
+                        btnLED1.BackColor = Color.GreenYellow;
+                    }
+                    break;
+                case 2:
+                    if (status == 0)
+                    {
+                        btnLED2.BackColor = Color.Gray;
+                    }
+                    else
+                    {
+                        btnLED2.BackColor = Color.GreenYellow;
+                    }
+                    break;
+                case 3:
+                    if (status == 0)
+                    {
+                        btnLED3.BackColor = Color.Gray;
+                    }
+                    else
+                    {
+                        btnLED3.BackColor = Color.GreenYellow;
+                    }
+                    break;
+                case 4:
+                    if (status == 0)
+                    {
+                        btnLED4.BackColor = Color.Gray;
+                    }
+                    else
+                    {
+                        btnLED4.BackColor = Color.GreenYellow;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void btnLED1_Click(object sender, EventArgs e)
+        {
+            string currentColorBtn = btnLED1.BackColor.ToString();
+            if(currentColorBtn.IndexOf("Gray") != -1)
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("11"));
+                btnLED1.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("10"));
+                btnLED1.BackColor = Color.Gray;
+            }
+        }
+
+        private void btnLED2_Click(object sender, EventArgs e)
+        {
+            string currentColorBtn = btnLED2.BackColor.ToString();
+            if (currentColorBtn.IndexOf("Gray") != -1)
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("21"));
+                btnLED2.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("20"));
+                btnLED2.BackColor = Color.Gray;
+            }
+        }
+
+        private void btnLED3_Click(object sender, EventArgs e)
+        {
+            string currentColorBtn = btnLED3.BackColor.ToString();
+            if (currentColorBtn.IndexOf("Gray") != -1)
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("31"));
+                btnLED3.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("30"));
+                btnLED3.BackColor = Color.Gray;
+            }
+        }
+
+        private void btnLED4_Click(object sender, EventArgs e)
+        {
+            string currentColorBtn = btnLED4.BackColor.ToString();
+            if (currentColorBtn.IndexOf("Gray") != -1)
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("41"));
+                btnLED4.BackColor = Color.GreenYellow;
+            }
+            else
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("40"));
+                btnLED4.BackColor = Color.Gray;
+            }
+        }
+
+        private void btnAllControl_Click(object sender, EventArgs e)
+        {
+            string textBtn = btnAllControl.Text;
+            if (textBtn.IndexOf("All ON") != -1)
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("01"));                
+                btnLED1.BackColor = Color.GreenYellow;
+                btnLED2.BackColor = Color.GreenYellow;
+                btnLED3.BackColor = Color.GreenYellow;
+                btnLED4.BackColor = Color.GreenYellow;
+                btnAllControl.Text = "All OFF";
+            }
+            else
+            {
+                client.Publish("Desktop/LEDControl", Encoding.UTF8.GetBytes("00"));                
+                btnLED1.BackColor = Color.Gray;
+                btnLED2.BackColor = Color.Gray;
+                btnLED3.BackColor = Color.Gray;
+                btnLED4.BackColor = Color.Gray;
+                btnAllControl.Text = "All ON";
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+            client.Disconnect();
+        }
+    }    
+}
